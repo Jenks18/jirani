c478fafe397ed129ba03b073844de8459900da67
Ian Njenga <iannjesh@gmail.com>
2025-09-03 19:29:15 -0400

    Remove old conflicting API files

--- api/store-report.js
+++
@@ -1,5 +0,0 @@
-// Stores data (MVP: just returns success)
-export default function handler(req, res) {
-  // TODO: Store report in DB or file
-  res.status(200).json({ status: 'Report stored' });
-}
