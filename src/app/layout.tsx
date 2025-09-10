68f502297015fbc8d99aea5083f6caa28c1a6760
Ian Njenga <iannjesh@gmail.com>
2025-09-09 21:59:33 -0400

    Fix Mapbox CSS - use CDN in head tag and remove local import

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -24,6 +24,12 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en">
+      <head>
+        <link
+          href="https://api.mapbox.com/mapbox-gl-js/v3.0.1/mapbox-gl.css"
+          rel="stylesheet"
+        />
+      </head>
       <body
         className={`${geistSans.variable} ${geistMono.variable} antialiased`}
       >
